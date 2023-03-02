fc19ba9e72dea668469708e9cfb7105023b79e33
Subas Adhikari <subasadhikari0025@gmail.com>
2023-03-02 12:43:43 +0545

    Segmentation Need to work for average case

--- copy_of_charactersegementation.py
+++ copy_of_charactersegementation.py
@@ -3,6 +3,7 @@ from keras.models import model_from_json
 from keras.models import load_model
 import matplotlib.pyplot as plt
 import cv2
+import math
 
 import warnings
 
@@ -24,7 +25,6 @@ class Recog:
             imgxwithpadding = np.zeros((32, 32), np.uint8)
             imgxwithpadding[1:30, 1:30] = roi
             roi = imgxwithpadding
-
             roi = np.array(roi)
             roi.reshape(1, 1024)
             prediction = loaded_model.predict(roi.reshape(1, 1024))
@@ -48,14 +48,15 @@ class Recog:
         return classifier(img)
 
     def Segmentation(self, ximg, counter):
-        keys2, keys, ximg = self.getVerticalProjectionProfile(ximg)
+        segmentingPosition = self.getVerticalProjectionProfile(ximg)
+
+        ximg = cv2.bitwise_not(ximg)
         copyForSegmentationShow = ximg.copy()
         char = ""
         segmentedimages = []
-        for key in range(0, len(keys) - 1):
-            imgx = ximg[0:ximg.shape[0], keys[key] - 2:(keys2[key + 1] + keys[key + 1]) // 2 + 4]
-            cv2.rectangle(copyForSegmentationShow, (keys[key] - 2, 0),
-                          ((keys2[key + 1] + keys[key + 1]) // 2 + 4, ximg.shape[0]), (255, 0, 0), 1)
+        for pos in range(0, len(segmentingPosition)-1):
+            imgx = ximg[0:ximg.shape[0], segmentingPosition[pos] :segmentingPosition[pos + 1] ]
+            cv2.rectangle(copyForSegmentationShow, (segmentingPosition[pos], 0),(segmentingPosition[pos + 1], ximg.shape[0]), (255, 0, 0), 1)
             sum = 0
             for i in imgx:
                 for j in imgx:
@@ -78,19 +79,32 @@ class Recog:
         org = ximg.copy()
         kernel = np.ones((5, 5), np.float32) / 30
         ximg = cv2.filter2D(ximg, -1, kernel)
-        ximg = cv2.rotate(ximg,cv2.ROTATE_90_CLOCKWISE)
-        std = {}
-        positions  = {i:[] for i in range(0,ximg.shape[0])}
-        plt.imshow(ximg)
-        plt.show()
-        for i in range(0,len(ximg)):
-            for j in range(0,len(ximg[0])):
-                if ximg[i][j] > 100:
-                    positions[i].append(j)
 
-        for rowkey in positions:
-            std[rowkey] = np.std(positions[rowkey])
-        print(std)
+        segmentingPosition = self.getSegmentationPosition(ximg)
+        return segmentingPosition
+
+
+    def getSegmentationPosition(self, ximg):
+        std, average = self.getStdandAverageofVericallines(ximg)
+        sum = self.getSumofVerticalLines(ximg)
+        x = list(set(sum.values()))
+        averageValues = [x if not math.isnan(x) else -1 for x in average.values()]
+        sortedAverage = sorted(set(sorted(list(averageValues))[::-1]))[::-1]
+        resultOfStd = {key: val for key, val in std.items() if val > 3}
+        print(resultOfStd)
+        resultOfAverage = {key: val for key, val in average.items() if val < min(sortedAverage[:10])}
+        commonKeys = sorted(
+            set(resultOfStd.keys()).intersection(set(resultOfAverage.keys())).union(set(resultOfStd.keys())))
+        discontinuities = self.find_discontinuities(commonKeys)
+        segmentingPosition = [0]
+        for each in discontinuities:
+            segmentingPosition.append((each[0] + each[1]) // 2)
+        segmentingPosition.append(ximg.shape[1])
+        print(segmentingPosition)
+        return segmentingPosition
+
+
+    def getSumofVerticalLines(self, ximg):
         dict = {}
         for i in range(0, len(ximg[0])):
             sum = 0
@@ -98,33 +112,49 @@ class Recog:
                 if ximg[j][i] > 100:
                     sum += 1
             dict[i] = sum
-        x = list(set(dict.values()))
-        print(dict)
-        greater_than_threshold = {k: v for k, v in dict.items() if v >= max(x[0:5]) * 1.25}
-        current_key = min(greater_than_threshold.keys())
-        current_part = {current_key: greater_than_threshold[current_key]}
-        parts = [current_part]
-        keys = []
-        for key in sorted(greater_than_threshold.keys()):
-            if key == current_key + 1:
-                current_key = key
-                current_part[key] = greater_than_threshold[key]
+        return dict
+
+    def getStdandAverageofVericallines(self, ximg):
+        std = {}
+        average = {}
+        positions = self.GetVerticalPositions(ximg)
+        for rowkey in positions:
+            std[rowkey] = np.std(positions[rowkey])
+            average[rowkey] = np.mean(positions[rowkey])
+        return std, average
+
+    def GetVerticalPositions(self, ximg):
+        ximg = cv2.rotate(ximg, cv2.ROTATE_90_CLOCKWISE)
+        positions = {i: [] for i in range(0, ximg.shape[0])}
+        plt.imshow(ximg)
+        plt.show()
+        for i in range(0, len(ximg)):
+            for j in range(0, len(ximg[0])):
+                if ximg[i][j] > 100:
+                    positions[i].append(j)
+        ximg = cv2.rotate(ximg, cv2.ROTATE_90_COUNTERCLOCKWISE)
+        return positions
+
+    def find_discontinuities(self, nums):
+        start = end = None
+        discontinuities = []
+
+        for i in range(len(nums) - 1):
+            if nums[i + 1] != nums[i] + 1:
+                if start is None:
+                    start = nums[i]
+                end = nums[i + 1]
             else:
-                current_key = key
-                keys.append(key)
-                current_part = {current_key: greater_than_threshold[key]}
-                parts.append(current_part)
-        keys.append(ximg.shape[1])
-        keys2 = []
-        for key in sorted(greater_than_threshold.keys(), reverse=True):
-            if key == current_key - 1:
-                current_key = key
-                current_part[key] = greater_than_threshold[key]
-            else:
-                current_key = key
-                keys2.append(key)
-                current_part = {current_key: greater_than_threshold[key]}
-                parts.append(current_part)
-        keys2.append(0)
-        ximg = org
-        return keys2[::-1], keys, ximg
+                if start is not None:
+                    discontinuities.append((start, end))
+                    start = end = None
+
+        # If the loop ends with a discontinuity
+        if start is not None:
+            discontinuities.append((start, end))
+        finaldiscontinuities = []
+        for each in discontinuities:
+            if each[1] - each[0] > 3:
+                finaldiscontinuities.append(each)
+
+        return finaldiscontinuities
